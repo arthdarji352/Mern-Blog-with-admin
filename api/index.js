76afabee96c3a70e59474a5eb52c3d6590183a1d
arthdarji352 <arthdarji20@gmail.com>
2024-01-24 10:55:15 +0530

    add signup route and Error handler Middlerware

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 
+//database
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -15,8 +17,25 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/user", userRoutes);
+// middlewares
+app.use(express.json());
 
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+//server
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
