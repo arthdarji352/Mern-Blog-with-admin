5779f193c7e3a0b90ac8c72b16f01dc04b283c66
arthdarji352 <arthdarji20@gmail.com>
2024-01-28 23:18:59 +0530

    add user update API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,7 @@ mongoose
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 //routes
 app.use("/api/user", userRoutes);
