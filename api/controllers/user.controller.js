f91df9b20fd13f6cca559a48874bdf0382c61bc9
arthdarji352 <arthdarji20@gmail.com>
2024-01-29 18:01:56 +0530

    add User delete functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,6 +6,7 @@ export const test = (req, res) => {
   res.json({ message: "API is working!" });
 };
 
+//update USER
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to update this user"));
@@ -49,6 +50,19 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//DELETE USER
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
   }
